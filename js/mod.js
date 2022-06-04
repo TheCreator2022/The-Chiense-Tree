3d2f63a0c698a0b397ca2fae266c44df643053dd
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-06-04 11:07:53 -0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -45,6 +45,7 @@ function getPointGen() {
 	let gain = new Decimal(1)
 	if (hasUpgrade('一', 12)) gain = gain.add(5)
 	if (hasUpgrade('一', 13)) gain = gain.add(100)
+	if (hasUpgrade('一', 14)) gain = gain.add(2500)
 	return gain
 }
 
@@ -58,7 +59,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.points.gte(new Decimal("1e400"))
 }
 
 
