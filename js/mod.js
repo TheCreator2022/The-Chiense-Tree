f6da04a7dec44ce0b8d9afee17a8fe834fff137f
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-06-04 10:32:52 -0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -7,8 +7,8 @@ let modInfo = {
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+	initialStartPoints: new Decimal (90), // Used for hard resets and new players
+	offlineLimit: 10000,  // In hours
 }
 
 // Set your version in num and name
