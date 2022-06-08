a2cf60fc753f6d65fcbed1ba0eb27a30cf84a028
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-06-08 10:17:29 -0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("一", {
-    name: "U+4E00", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "一", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("C", {
+    name: "Coins", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "C", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -22,7 +22,7 @@ addLayer("一", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "c", description: "C: Reset for coin points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
